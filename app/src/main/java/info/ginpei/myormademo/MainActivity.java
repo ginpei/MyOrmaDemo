1e5f8485809d07b8ef99650622d0ff1cbc40f6d8
ginpei <ginpei@gmail.com>
2017-03-15 15:53:56 -0700

    update all users

--- app/src/main/java/info/ginpei/myormademo/MainActivity.java
+++ app/src/main/java/info/ginpei/myormademo/MainActivity.java
@@ -10,6 +10,7 @@ import info.ginpei.myormademo.basicModels.OrmaDatabase;
 import info.ginpei.myormademo.basicModels.User;
 import info.ginpei.myormademo.basicModels.User_Relation;
 import info.ginpei.myormademo.basicModels.User_Selector;
+import info.ginpei.myormademo.basicModels.User_Updater;
 import info.ginpei.myormademo.util.InputDialogBuilder;
 
 public class MainActivity extends AppCompatActivity {
@@ -186,6 +187,69 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         Toast.makeText(MainActivity.this, "Read!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }).start();
+    }
+
+
+    /**
+     * Called from a button.
+     *
+     * @param view Button view.
+     */
+    public void updateAllButton_click(View view) {
+        // show a dialog and receive new user's name
+        InputDialogBuilder builder = new InputDialogBuilder(this);
+        builder.setTitle("Update");
+        builder.setMessage("Input new name for all users");
+        builder.setHint("Alice");
+        builder.setCallback(new InputDialogBuilder.Callback() {
+            @Override
+            public void onClick(String result) {
+                if (result != null && !result.isEmpty()) {
+                    // OK let's make it!
+                    updateAllUsers(result);
+                }
+            }
+        });
+        builder.show();
+    }
+
+    /**
+     * Update all rows in the user table.
+     *
+     * @param userName New name.
+     */
+    private void updateAllUsers(final String userName) {
+        // Q: Why final?
+        // A: Because they are used in the other thread.
+        //    You need to make sure they won't change since the thread runs asynchronously.
+
+        // prepare Orma
+        OrmaDatabase orma = OrmaDatabase.builder(this).build();
+        final User_Relation userRelation = orma.relationOfUser();
+
+        // select the models from your database
+        // (It needs to run on a worker thread.)
+        Log.d(TAG, "updateAllUsers: Updating...");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                User_Updater userUpdater = userRelation.updater();
+                userUpdater
+                        .name(userName)
+                        .execute();
+
+                Log.d(TAG, "updateAllUsers: Updated.");
+
+                // show the result
+                // (It needs to run on the UI thread. :D )
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "Updated!", Toast.LENGTH_SHORT).show();
                     }
                 });
             }
